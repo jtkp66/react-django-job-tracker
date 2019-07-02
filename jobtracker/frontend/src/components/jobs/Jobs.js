cd40254d8cb81c4ba4e49691f0373e11d670084c
Jamie Pham <jtkp66@gmail.com>
2019-07-01 18:11:06 -0700

    Created Dasboard componnents, and form and job list sub components to front end

---
+++ jobtracker/frontend/src/components/jobs/Jobs.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Jobs extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Job App List</h1>
+      </div>
+    );
+  }
+}
+
+export default Jobs;
